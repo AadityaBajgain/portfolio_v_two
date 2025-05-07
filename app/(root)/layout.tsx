2a8baf89fd513869aff93ba5d7f955b26aed8c12
AadityaBajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-05-07 13:09:56 -0700

    remove favicon and clean up global styles; add layout and home page components

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from 'react'
+
+const layout = ({children}:{children:ReactNode}) => {
+  return (
+    <div>
+      hello world
+      
+    </div>
+  )
+}
+
+export default layout
