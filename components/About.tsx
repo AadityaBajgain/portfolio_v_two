194ab6597e302cb2308572a0805c270def8ebcf4
AadityaBajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-05-09 15:55:38 -0700

    refactor About and Navbar components; enhance layout, add responsive menu, and update package dependencies

--- components/About.tsx
+++ components/About.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import Image from 'next/image'
+
 const About = () => {
-    return (
-        <section className='w-full flex flex-row justify-around items-center gap-10'>
-            <Image src="/Portfolio.webp" alt="photo" width={200} height={100} className='rounded-full opacity-90'/>
-            <div>
-                <h1 className="text-4xl font-bold ">Welcome to My Portfolio</h1>
-                <p className="text-lg mt-5">I am a passionate web developer with a knack for creating dynamic and responsive web applications. My journey in web development has been fueled by a love for technology and a desire to create impactful digital experiences.</p>
-            </div>
-        </section>
-    )
+  return (
+    <section className="w-full px-6 py-10 flex flex-row md:flex-row justify-around items-center gap-10">
+      <Image
+        src="/Portfolio.webp"
+        alt="photo"
+        width={200}
+        height={200}
+        className="rounded-full opacity-90 object-cover"
+      />
+      <div className="text-center md:text-left max-w-xl">
+        <h1 className="text-3xl md:text-4xl font-bold">Welcome to My Portfolio</h1>
+        <p className="text-base md:text-lg mt-4">
+          I am a passionate web developer with a knack for creating dynamic and responsive web applications. My journey in web development has been fueled by a love for technology and a desire to create impactful digital experiences.
+        </p>
+      </div>
+    </section>
+  )
 }
 
 export default About
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,58 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
+import { FaBars, FaTimes } from 'react-icons/fa'
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <nav className='flex justify-between items-center mt-4 mb-4'>
-        <h1><Link href="/">Aaditya Bajgain</Link></h1>
-        <div>
-            <ul className='flex flex-row gap-4 text-sm'>
-              <li>About</li>
-              <li>Projects</li>
-              <li>Experience</li>
-              <li>Contact</li>
-            </ul>
-        </div>
+    <nav className="px-4 py-4 flex justify-between items-center">
+      <h1 className="text-lg md:text-2xl font-bold">
+        <Link href="/">Aaditya Bajgain</Link>
+      </h1>
+
+      
+      <div className="cursor-pointer md:hidden z-20" onClick={toggleMenu} >
+        {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+      </div>
+
+      
+      <ul className="hidden md:flex gap-4 text-sm md:text-base">
+        <li className="hover:text-blue-500 transition-colors">
+          <Link href="/about">About</Link>
+        </li>
+        <li className="hover:text-blue-500 transition-colors">
+          <Link href="/projects">Projects</Link>
+        </li>
+        <li className="hover:text-blue-500 transition-colors">
+          <Link href="/experience">Experience</Link>
+        </li>
+        <li className="hover:text-blue-500 transition-colors">
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+
+      {isOpen && (
+        <ul className="absolute top-16 right-4 w-full rounded-b-lg bg-white/30 dark:bg-black/30 backdrop-blur-md flex flex-col items-center gap-4 py-6 text-base shadow-md z-10 md:hidden transition-transform transform-fillS duration-500">
+          <li onClick={toggleMenu}>
+            <Link href="/about">About</Link>
+          </li>
+          <li onClick={toggleMenu}>
+            <Link href="/projects">Projects</Link>
+          </li>
+          <li onClick={toggleMenu}>
+            <Link href="/experience">Experience</Link>
+          </li>
+          <li onClick={toggleMenu}>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      )}
     </nav>
   )
 }
