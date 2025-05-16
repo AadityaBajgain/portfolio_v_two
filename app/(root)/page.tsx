69e019c1982ac10d749f648d63d6b0e4cd850686
AadityaBajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-05-15 18:46:50 -0700

    refactor: restructure Home component to include Experience; update styles for Education and Experience entries

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,13 +2,18 @@ import About from '@/components/About';
 import Tools from '@/components/Tools';
 import Github from '@/components/Github';
 import Education from '@/components/Education';
+import Experience from '@/components/Experience';
 export default function Home() {
   return (
     <main className="flex flex-col max-h-screen py-10 sm:py-20">
-      
+
       <About />
       {/* <Github/> */}
-      <Education/>
+      <div className='flex flex-col md:flex-wrap md:gap-5'>
+        <Education />
+        <Experience />
+      </div>
+
       {/* <ShowRepo repos={[]}/> */}
       {/* <Tools /> */}
     </main>
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Experience: React.FC = () => {
   return (
-    <div className="p-4">
-      <h2 className="text-2xl font-bold mb-4">Experience</h2>
-      <div className="mb-6">
-        <h3 className="text-xl font-semibold">
+    <div className="experience p-4">
+      <h2>Experience</h2>
+      <div >
+        <h3>
           Student Developer | Self-Driven Projects
         </h3>
-        <ul className="list-disc list-inside mt-2">
+        <ul>
           <li>Built and tested responsive, accessible, and secure web apps using SDLC principles.</li>
           <li>Managed basic QA workflows, including test documentation and validation on staging branches.</li>
         </ul>
