0366c78add1bebc2f4e1bf83b940a851beff2301
AadityaBajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-05-29 01:55:26 -0700

    made some changes in github's graphql query and aligned all components design

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import About from '@/components/HeroSection';
 import Education from '@/components/Education';
 import Experience from '@/components/Experience';
-// import Projects from '@/components/Projects';
+import Projects from '@/components/Projects';
 import CodingNow from '@/components/CurrentProjects';
 
 export default function Home() {
@@ -13,7 +13,7 @@ export default function Home() {
 
       {/* Experience & Education */}
       <section className="section-container">
-        <h1 className=''>About Me</h1>
+        <h1 className='mb-5'>About Me</h1>
         <div className="grid md:grid-cols-2 gap-8">
           <Education />
           <Experience />
@@ -21,9 +21,9 @@ export default function Home() {
       </section>
 
       <section className="section-container">
-        <h1 className=''>My Projects</h1>
+        <h1 className='mb-5'>My Projects</h1>
         <div className="grid md:grid-cols-2 gap-8">
-          {/* <Projects /> */}
+          <Projects />
           <CodingNow />
         </div>
 
