656c728586152f9ec1a9cbd2c794329dea7f91e3
AadityaBajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-06-10 11:43:26 -0700

    maintained the aspect ration of image

--- components/Tools.tsx
+++ components/Tools.tsx
@@ -2,10 +2,13 @@
 
 import React from "react";
 import Image from "next/image";
-
+const style = {
+  "width":"auto",
+  "height":"auto"
+}
 const Tools = () => {
   return (
-    <div className="p-6 md:p-8 space-y-6 flex flex-wrap gap-10 justify-between">
+    <div className="p-6 flex flex-wrap md:p-8 space-y-6 gap-10 justify-between">
       <div>
         <h2 className="gradient-text">Languages that I use</h2>
         <div className="mt-5 flex flex-col gap-5">
@@ -13,18 +16,20 @@ const Tools = () => {
             <Image
               src="https://skillicons.dev/icons?i=js,ts,py,cpp,c"
               alt="Programming Languages: JavaScript, TypeScript, Python, C++, C"
-              width={240}
-              height={48}
+              width={100}
+              height={100}
               unoptimized
+              style={style}
             />
           </div>
           <div>
             <Image
               src="https://skillicons.dev/icons?i=html,css"
               alt="Web Technologies: HTML, CSS"
-              width={96}
-              height={48}
+              width={100}
+              height={100}
               unoptimized
+              style={style}
             />
           </div>
         </div>
@@ -37,9 +42,10 @@ const Tools = () => {
           <Image
             src="https://skillicons.dev/icons?i=react,next,vite,tailwind,bootstrap"
             alt="Frameworks: React, Next.js, Vite, Tailwind CSS, Bootstrap"
-            width={240}
-            height={48}
+            width={100}
+            height={100}
             unoptimized
+            style={style}
           />
         </div>
       </div>
@@ -51,9 +57,25 @@ const Tools = () => {
           <Image
             src="https://skillicons.dev/icons?i=firebase,mongodb,mysql"
             alt="Databases: Firebase, MongoDB, MySQL"
-            width={144}
-            height={48}
+            width={100}
+            height={100}
             unoptimized
+            style={style}
+          />
+        </div>
+      </div>
+      <div>
+        <h2 className="gradient-text">
+          Tools that I use
+        </h2>
+        <div className="mt-5">
+          <Image
+            src="https://skillicons.dev/icons?i=vscode,git,github,postman"
+            alt="tools: vscode, git, github, postman"
+            width={100}
+            height={100}
+            unoptimized
+            style={style}
           />
         </div>
       </div>
