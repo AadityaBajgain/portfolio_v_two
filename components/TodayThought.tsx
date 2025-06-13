a1c9d6a2e10936ea732c61c1a503120aebbc39a9
AadityaBajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-06-13 16:12:06 -0700

    fix: remove unused import in TodayThought component

--- components/TodayThought.tsx
+++ components/TodayThought.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react'
 import { formatDistanceToNow } from 'date-fns'
-import { loadEnvFile } from 'process';
+
 
 interface StatusResponse {
   thoughts: string;
