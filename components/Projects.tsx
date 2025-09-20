20bc60a30b4ca63a84650b37308ea00e383a1bb2
Aaditya Bajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-09-20 14:37:17 -0700

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -77,7 +77,7 @@ const Projects: React.FC = () => {
               
               {repo.primaryLanguage && (
                 <div className="flex items-center gap-2 text-xs">
-                  <span
+                  Primary Language: <span
                     className="w-3 h-3 rounded-full"
                     style={{ backgroundColor: repo.primaryLanguage.color }}
                   />
