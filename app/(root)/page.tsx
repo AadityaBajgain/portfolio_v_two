d4434dec3a1ba2d0bca922da30298b2444099785
Aaditya Bajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-10-23 16:38:25 -0700

    Change section height to full screen

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,7 +11,7 @@ import Contact from '@/components/Contact';
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <section className="section-container flex flex-col">
+      <section className="section-container h-screen flex flex-col">
         <TodayThought/>
         <HeroSection />
       </section>
