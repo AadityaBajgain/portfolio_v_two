152c4c26c2704c8ae7b9c46bf5706136d99daf54
Aaditya Bajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-10-23 16:43:30 -0700

    Add margin-top to HeroSection for larger screens

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 const HeroSection:React.FC = () => {
   return (
-    <section className="section-container mx-auto gap-10 flex flex-col items-center md:gap-0 ">
+    <section className="section-container mx-auto gap-10 flex flex-col items-center md:gap-0 lg:mt-10">
       <div className='flex flex-col md:flex-row justify-around items-center gap-10'>
         <Image
           src="/pic.webp"
